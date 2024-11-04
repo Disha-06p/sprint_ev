aa84e7d4b05e421b17e8ca4f42b4e6b312369f02
disha <dishaparmesh06@gmail.com>
2024-11-04 21:12:32 +0530

    add functionality to add two numbers

---
+++ src/main/java/com/example/demo/SprintPracticeGit1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintPracticeGit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintPracticeGit1Application.class, args);
+	}
+
+}
